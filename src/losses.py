be7429a39317d0ea13c141d20ac2949818db505f
ConzumexGame <jojigeorge95@gmail.com>
2026-07-10 18:48:04 +0530

    losses function test added

--- src/losses.py
+++ src/losses.py
@@ -8,10 +8,39 @@ class MSELoss:
 
 
     def forward(self, y_pred, y_true):
+        self.y_pred = y_pred
+        self.y_true = y_true
+
         loss = np.mean((y_true - y_pred)**2)
         return loss
 
-    def backward(self):
-        pass
 
 
+class BCELoss:
+    def __init__(self):
+        self.y_pred = None
+        self.y_true = None
+
+    def forward(self, y_pred, y_true):
+    
+        self.y_pred = np.clip(y_pred, 1e-15, 1-1e-15)
+        self.y_true = y_true
+
+        loss = -np.mean(self.y_true*np.log(self.y_pred) + (1 - self.y_true)*np.log(1 - self.y_pred))
+        return loss
+
+
+class CrossEntropyLoss:
+    def __init__(self):
+        self.y_pred = None
+        self.y_true = None
+
+    def forward(self, y_pred, y_true):
+    
+        
+        
+        self.y_pred = np.clip(y_pred, 1e-15, 1-1e-15)
+        self.y_true = y_true
+
+        loss = -np.mean(self.y_true*np.log(self.y_pred) + (1 - self.y_true)*np.log(1 - self.y_pred))
+        return loss
--- tests/test_losses.py
+++ tests/test_losses.py
@@ -1,5 +1,5 @@
 import numpy as np
-from src.losses import MSELoss
+from src.losses import MSELoss, BCELoss
 
 
 def test_mse_loss_forward():
@@ -14,3 +14,13 @@ def test_mse_loss_forward():
     # 3. Assert (Check if the result matches the expected hand-calculated math)
     # Expected: ((3-2)^2 + (3-4)^2 + (9-7)^2) / 3 = (1 + 1 + 4) / 3 = 2.0
     assert np.isclose(calculated_loss, 2.0), f"Expected 2.0 but got {calculated_loss}"
+
+
+def test_bse_loss_forward():
+    loss_fn = BCELoss()
+    y_pred = np.array([[0.5], [1.0], [0.8]])
+    y_true = np.array([[1.0], [0.0], [1.0]])
+
+    calculated_loss = loss_fn.forward(y_pred, y_true)
+    expected_loss = 11.8186
+    assert np.isclose(calculated_loss, expected_loss), f"Expected {expected_loss} but got {calculated_loss}"
